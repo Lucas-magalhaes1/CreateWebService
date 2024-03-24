bce0cdbd5fc164bded027ef5add5d02f8cea5bfa
Lucas <lucasmagalhaes728@gmail.com>
2024-03-23 21:34:35 -0300

    Adicionar arquivos de projeto.

---
+++ WebService/MDCCliente.cs
@@ -0,0 +1,35 @@
+﻿using CreatWebService.Sevice;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Services.Protocols;
+
+namespace MDCClient
+{
+    internal class MDCClient
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                MDCWebService.MDCCalculator client = new MDCWebService.MDCCalculator();
+                int x = 640; // Largura 
+                int y = 480; // Altura 
+                int mdc = client.CalculateMDC(x, y);
+                double aspectRatioX = (double)x / mdc;
+                double aspectRatioY = (double)y / mdc;
+                Console.WriteLine($"Aspect Ratio: {aspectRatioX}:{aspectRatioY}");
+            }
+            catch (SoapException ex)
+            {
+                Console.WriteLine($"SOAP Exception: {ex.Message}");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Exception: {ex.Message}");
+            }
+        }
+    }
+}
---
+++ WebService/Service/MDCWebService.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Services;
+
+namespace CreatWebService.Sevice
+{
+    internal class MDCWebService
+    {
+        [WebService(Namespace = "http://localhost:3000/mdc")]
+        [WebServiceBinding(ConformsTo = WsiProfiles.BasicProfile1_1)]
+        [System.ComponentModel.ToolboxItem(false)]
+        public class MDCCalculator : System.Web.Services.WebService
+        {
+            [WebMethod]
+            public int CalculateMDC(int x, int y)
+            {
+                while (y != 0)
+                {
+                    int temp = y;
+                    y = x % y;
+                    x = temp;
+                }
+                return x;
+            }
+        }
+    }
+}
+
